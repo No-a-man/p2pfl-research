02fc8f5fa830dab8ba90b6ebc3b62f0d513c8245
Noaman Rukdikar <69784156+No-a-man@users.noreply.github.com>
2025-10-11 13:56:30 -0400

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,25 +1,3 @@
-#
-# This file is part of the federated_learning_p2p (p2pfl) distribution
-# (see https://github.com/pguijas/p2pfl).
-# Copyright (c) 2022 Pedro Guijas Bravo.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-
-"""Example of a P2PFL MNIST experiment, using a MLP model and a MnistFederatedDM."""
-
-# poetry run snakeviz _MainThread-0.pstat
-# poetry run gprof2dot -f pstats Gossiper-10.pstat | dot -Tpng -o output.png && open output.png
 
 import argparse
 import time
